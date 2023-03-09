190ebc85e2f810e2da8d75c9184cf84fbb426560
Mert Kantaroglu <mertkantaroglu@gmail.com>
2023-03-09 16:13:34 +0300

    Fix linter errors

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Categories from './routes/Categories';
+import Books from './routes/Books';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div>Bookstore App</div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Books />} />
+        <Route path="/categories" element={<Categories />} />
+        <Route path="/*" element={<h1>Page Not Found!</h1>} />
+      </Routes>
+    </>
   );
 }
 
