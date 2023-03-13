d56ac9299d8c236ad34f81f6949016cfbbe73131
Mert Kantaroglu <mertkantaroglu@gmail.com>
2023-03-13 21:16:00 +0300

    Fix PR errors

--- src/redux/categories/categoriesSlice.js
+++ src/redux/categories/categoriesSlice.js
@@ -2,17 +2,17 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   categories: [],
-  status: 'Under construction',
 };
 
 export const categoriesSlice = createSlice({
   name: 'categories',
   initialState,
   reducers: {
-    checkStatus: (state, action) => ({
-      ...state,
-      status: action.payload,
-    }),
+    checkStatus: (state, action) => {
+      state.categories = action.payload === 'Under construction'
+        ? 'Under construction'
+        : state.categories;
+    },
   },
 });
 
